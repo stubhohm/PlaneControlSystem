85956aa976b3cb76df5bbafe2e36c0b1b148c153
stubhohm <stubhohm@gmail.com>
2024-09-19 23:09:32 -0500

    need to add in passing of sensor data to the parts that use internal feedback loops such as the nozzle or the airspike to determine positioning.

--- Classes/InputClasses/LandingGear.py
+++ Classes/InputClasses/LandingGear.py
@@ -4,4 +4,4 @@ class LandingGear():
     def __init__(self) -> None:
         self.name = StrAtt('Landing Gear')
         self.stow_gear = BoolAtt(False)
-        self.brakes = BoolAtt()
+        self.brakes = BoolAtt(True)
